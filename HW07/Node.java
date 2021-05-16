cab42fb412ffac4e7bf3b4316993e00a7894d814
bambithenambi <abhijitvnambiar@gmail.com>
2021-05-16 10:44:38 -0700

    Node Compiles

---
+++ HW07/Node.java
@@ -0,0 +1,25 @@
+public class Node<T> {
+    private T data;
+    private Node<T> next;
+
+    Node(T data) {
+        this(data, null);
+    }
+    Node(T data, Node<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public T getData(){
+        return data;
+    }
+    public void setData(T data){
+        this.data = data;
+    }
+    public Node<T> getNext(){
+        return next;
+    }
+    public void setNext(Node<T> next){
+        this.next = next;
+    }
+}
